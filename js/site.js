63a2a9ceadedb3c4db1e5e9bd9c3903b2d01b27b
kurtiskronk <kurtis@kurtiskronk.com>
2026-09-03 21:07:44 -0500

    Initial custom code structure update with purging built-in

--- js/site.js
+++ js/site.js
@@ -1,3 +1,12 @@
 console.log(
 	'Mark Thomas Films: site.js loaded from GitHub/jsDelivr.'
 );
+
+
+/* ==================================================
+	 LOADER UPDATE TEST
+	 ================================================== */
+
+window.alert(
+	'Mark Thomas Films loader test — latest GitHub code loaded successfully.'
+);

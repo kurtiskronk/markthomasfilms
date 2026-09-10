292ba993d8359d751147f243dca45b20f5d67f67
kurtiskronk <kurtis@kurtiskronk.com>
2026-09-10 00:15:37 -0500

    fixed referral name not showing up

--- js/site.js
+++ js/site.js
@@ -143,7 +143,7 @@ console.log(
 			referralName:
 				fieldValue(
 					form,
-					'#text-3fd04055-f9e4-4830-b67b-522f09f488e3-field'
+					'#text-0e6d3a16-2749-434e-9cc4-317142fda434-field'
 				),
 
 			message:

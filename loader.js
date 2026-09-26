c8717d9f5ad67a709ddcb40b8f0552dcb4b6169e
kurtiskronk <kurtis@kurtiskronk.com>
2026-09-25 21:38:13 -0500

    css minor tweak blog header

--- loader.js
+++ loader.js
@@ -12,7 +12,7 @@
 		'https://markthomasfilms.mark-a7f.workers.dev';
 
 	const assetVersion =
-		'2026-09-26-blog-layout-r1';
+		'2026-09-26-blog-layout-r2';
 
 	const jsFiles = [
 		'js/films-tag-context.js',

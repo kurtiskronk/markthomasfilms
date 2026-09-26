29e99d3e1d573f78ffb6071ff00ceb2b27764ac9
kurtiskronk <kurtis@kurtiskronk.com>
2026-09-25 23:07:31 -0500

    updated loader.js revision

--- loader.js
+++ loader.js
@@ -12,7 +12,7 @@
 		'https://markthomasfilms.mark-a7f.workers.dev';
 
 	const assetVersion =
-		'2026-09-26-blog-layout-r5';
+		'2026-09-26-blog-layout-r6';
 
 	const jsFiles = [
 		'js/films-tag-context.js',
